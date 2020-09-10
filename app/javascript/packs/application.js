ecec8409ea9a22c20c6a858fcb0488e5e9407f60
nenekelvin <karvinmitchar@gmail.com>
2020-09-10 11:07:44 +0300

    use webpack helpers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,10 @@
 console.log('Hello World from Webpacker')
 
 import "controllers"
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
