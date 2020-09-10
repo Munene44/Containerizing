d4e06962a6189f3426746d10d3847edc7857d0aa
nenekelvin <karvinmitchar@gmail.com>
2020-09-10 10:13:09 +0300

    create nested resource post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :sharks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'sharks#index'
