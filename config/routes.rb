2de4d289fc1f14a454c709569f5a711c0fe76062
nenekelvin <karvinmitchar@gmail.com>
2020-09-10 10:17:11 +0300

    setup up rlship in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :sharks
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sharks do
+    resources :posts
+  end
   root 'sharks#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
