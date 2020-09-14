36d5e4f43cb01675c1bc9a94558c21ea34865232
nenekelvin <karvinmitchar@gmail.com>
2020-09-14 15:01:20 +0300

    define csv action

--- app/workers/add_endangered_worker.rb
+++ app/workers/add_endangered_worker.rb
@@ -3,4 +3,11 @@ class AddEndangeredWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
 
+  
+  def perform(csv_file)
+    CSV.foreach(csv_file, headers: true) do |shark|
+    Endangered.create(name: shark[0], iucn: shark[1])
+  end
+ end
+
 end
