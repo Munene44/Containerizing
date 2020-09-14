efa1b64fffbf70aa1cc69301a738423386ed6708
nenekelvin <karvinmitchar@gmail.com>
2020-09-14 15:10:37 +0300

    :penci:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_071136) do
+ActiveRecord::Schema.define(version: 2020_09_14_074410) do
+
+  create_table "endangereds", force: :cascade do |t|
+    t.string "name"
+    t.string "iucn"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
