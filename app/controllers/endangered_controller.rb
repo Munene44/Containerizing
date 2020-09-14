c2e6ea196c2e70218e3d36e726bfb5a64f009193
nenekelvin <karvinmitchar@gmail.com>
2020-09-14 10:51:17 +0300

    add instances

--- app/controllers/endangered_controller.rb
+++ app/controllers/endangered_controller.rb
@@ -1,4 +1,17 @@
 class EndangeredController < ApplicationController
+	 before_action :set_endangered, only: [:index, :data]
   def index
+  	if @endangered.length > 0
+      redirect_to endangered_data_path
+    else
+      render 'index'
+    end
   end
+
+
+  private
+
+    def set_endangered
+      @endangered = Endangered.all 
+    end
 end
